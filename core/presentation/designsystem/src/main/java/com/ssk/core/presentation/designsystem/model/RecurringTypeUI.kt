85c0afbf73537d25c2ba0713aea58eaf277aaafb
Seeker1805 <kulkarnisameer1809@gmail.com>
2025-04-17 21:14:16 -0400

    Fix date display for MONTHLY and YEARLY types in RecurringTypeUI; append 'th' to day representation for clarity

--- core/presentation/designsystem/src/main/java/com/ssk/core/presentation/designsystem/model/RecurringTypeUI.kt
+++ core/presentation/designsystem/src/main/java/com/ssk/core/presentation/designsystem/model/RecurringTypeUI.kt
@@ -6,6 +6,6 @@ enum class RecurringTypeUI(val symbol: String, val title: String) {
     ONE_TIME("\uD83D\uDD04", "Does not repeat"),
     DAILY("\uD83D\uDD04", "Daily"),
     WEEKLY("\uD83D\uDD04", "Weekly on ${InstantFormatter.getCurrentDayOfWeek()}"),
-    MONTHLY("\uD83D\uDD04", "Monthly on the ${InstantFormatter.getCurrentDayOfMonth()}"),
-    YEARLY("\uD83D\uDD04", "Yearly on ${InstantFormatter.getCurrentMonthAndDay()}")
+    MONTHLY("\uD83D\uDD04", "Monthly on the ${InstantFormatter.getCurrentDayOfMonth()}th"),
+    YEARLY("\uD83D\uDD04", "Yearly on ${InstantFormatter.getCurrentMonthAndDay()}th")
 }
